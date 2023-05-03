8c46e79910ba8f8c477ffdcdad09fbe4f0a6c07c
AnmolKeCode <130210804+AnmolKeCode@users.noreply.github.com>
2023-05-03 20:51:20 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    backgroundImage: {
+      'Green-Leave': "url('/img/greenleaves.jpg')",
+    }
+
+  },
+  plugins: [],
+}
+
